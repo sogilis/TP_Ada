c3d7f467c69f2dd3e1cf33efd32172b58ce82ebb
ana <ana@sogilis.com>
2016-02-25 16:49:58 +0100

    Add History of an account

--- src/bank_account.adb
+++ src/bank_account.adb
@@ -4,21 +4,39 @@ procedure Bank_Account is
 
    type T_Euro is delta 0.01 range -100_000.00 .. 100_000.00;
 
+   type T_Operation is (Deposit, Withdraw, Transfer, NA);
+
+   type T_History is record
+      Operation : T_Operation := NA;
+      Amount : T_Euro := 0.00;
+      Transmitter : Natural := 0;
+      Payee : Natural := 0;
+   end record;
+
+   type A_History is array(1..10) of T_History;
+
    type T_Account is record
       Balance : T_Euro;
-      ID      : Positive;
+      ID      : Natural;
+      History : A_History;
+      Date    : Integer;
    end record;
+
 
    -------------------------------------------------------------------------------------------------
    procedure Deposit (Amount : T_Euro := 0.00; Account : in out T_Account) is
    begin
+      Account.Date := Account.Date + 1;
       Account.Balance := Account.Balance + Amount;
+      Account.History(Account.Date) := (Operation => Deposit, Amount => Amount, Transmitter => 0, Payee => 0);
    end Deposit;
 
    -------------------------------------------------------------------------------------------------
    procedure Withdraw (Amount : T_Euro := 0.00; Account : in out T_Account) is
    begin
+      Account.Date := Account.Date + 1;
       Account.Balance := Account.Balance - Amount;
+      Account.History(Account.Date) := (Operation => Withdraw, Amount => Amount, Transmitter => 0, Payee => 0);
    end Withdraw;
 
    -------------------------------------------------------------------------------------------------
@@ -42,17 +60,45 @@ procedure Bank_Account is
    -------------------------------------------------------------------------------------------------
    procedure Transfer (Amount : T_Euro := 0.00; Transmitter : in out T_Account; Payee : in out T_Account) is
    begin
-      Withdraw(Amount => Amount, Account => Transmitter);
-      Deposit(Amount => Amount, Account => Payee);
+      Transmitter.Date := Transmitter.Date + 1;
+      Payee.Date := Payee.Date + 1;
+      Transmitter.Balance := Transmitter.Balance - Amount;
+      Payee.Balance := Payee.Balance + Amount;
+      Transmitter.History(Transmitter.Date) := (Operation => Transfer, Amount => Amount, Transmitter => Transmitter.ID, Payee => Payee.ID);
+      Payee.History(Payee.Date) := (Operation => Transfer, Amount => Amount, Transmitter => Transmitter.ID, Payee => Payee.ID);
    end Transfer;
 
    -------------------------------------------------------------------------------------------------
+   procedure Display_History (Account : T_Account) is
+   begin
+      Put_Line("------------------------ History of account number " &Positive'Image(Account.ID) & " --------------------");
+      for i in Account.History'range loop
+         if Account.History(i).Operation = Transfer
+         then
+            Put("Operation : " &T_Operation'Image(Account.History(i).Operation));
+            Put(" of " &T_Euro'Image(Account.History(i).Amount) & " Euros");
+            Put(" from account number " &Positive'Image(Account.History(i).Transmitter));
+            Put_Line(" to account number " &Positive'Image(Account.History(i).Payee));
+         elsif Account.History(i).Operation = NA
+         then
+            null;
+         else
+            Put("Operation : " &T_Operation'Image(Account.History(i).Operation));
+            Put_Line(" of " &T_Euro'Image(Account.History(i).Amount) & " Euros");
+         end if;
+      end loop;
+   end Display_History;
+
+
    Account_1 : T_Account := T_Account'(Balance => 0.00,
-                                        ID      => 1);
+                                       ID      => 1,
+                                       History => <>,
+                                       Date => 0);
 
    Account_2 : T_Account := T_Account'(Balance => 0.00,
-                                        ID      => 2);
-
+                                       ID      => 2,
+                                       History => <>,
+                                       Date => 0);
 begin
 
    Deposit(Amount => 130.00, Account => Account_1);
@@ -64,7 +110,16 @@ begin
    Display_Creditor(Is_Account_Creditor(Account_2));
    Transfer(Amount => 20.0, Transmitter => Account_1, Payee => Account_2);
 
+
+   Transfer(Amount => 30.0, Transmitter => Account_2, Payee => Account_1);
+   Transfer(Amount => 10.0, Transmitter => Account_1, Payee => Account_2);
+   Transfer(Amount => 5.0, Transmitter => Account_2, Payee => Account_1);
+
    Display_Balance(Account_1);
    Display_Balance(Account_2);
+
+   Display_History(Account_2);
+   Display_History(Account_1);
+
 end Bank_Account;
 
