fefc1ab3a9843faf39329643db779bf39d0f7f44
ana <ana@sogilis.com>
2016-03-03 09:40:53 +0100

    Add P_Operation.adb

---
+++ src/P_operation.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+package body P_Operation is
+
+  procedure Display_Operation(Operation : T_Operation) is
+  begin
+ null;--  Put_Line("Operation of an amout of " & T_Euro'Image (Operation.Amount));
+  end Display_Operation;
+
+end P_Operation;
